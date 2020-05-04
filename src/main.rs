4b2454539d8d997179cedffc92c1c54304d5d4c3
Jonathan Neufeld <jneufeld@alumni.ubc.ca>
2020-05-03 18:27:56 -0700

    Add vim swap files to gitignore
    Remove them from the repo.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@ use std::process::Command;
 use std::str;
 
 fn main() {
+    let git_log = get_git_logs();
+
+    println!("{}", &git_log);
+}
+
+fn get_git_logs() -> String {
     let proc_output = Command::new("git")
         .arg("log")
         .arg("--stat")
         .output()
         .unwrap();
 
-    let git_log = str::from_utf8(&proc_output.stdout).unwrap();
+    let git_logs = str::from_utf8(&proc_output.stdout).unwrap();
+    let git_logs = git_logs.to_string();
 
-    println!("{}", &git_log);
+    git_logs
 }
