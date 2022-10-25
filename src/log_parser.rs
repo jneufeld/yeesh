ca68ae3f534b23be9e8bf530c0e0bc98c07a51ca
Jonathan Neufeld <jneufeld@alumni.ubc.ca>
2022-10-25 12:10:48 -0700

    Update according to `fmt` and `clippy`
    No functional changes

--- src/log_parser.rs
+++ src/log_parser.rs
@@ -18,42 +18,42 @@ pub fn to_commits(logs: &str) -> Vec<Commit> {
     for line in logs.split('\n') {
         let mut is_commit_ready = false;
 
-        match get_one(&re_hash, &line) {
+        match get_one(&re_hash, line) {
             None => (),
             Some(h) => commit.hash = h,
         };
 
-        match get_two(&re_auth, &line) {
+        match get_two(&re_auth, line) {
             None => (),
             Some((name, email)) => {
                 commit.name = name;
                 commit.email = email;
-            },
+            }
         };
 
-        match get_one(&re_date, &line) {
+        match get_one(&re_date, line) {
             None => (),
             Some(d) => {
                 let date_time = DateTime::parse_from_rfc2822(&d);
-                let date_time = date_time.expect(&format!("Unable to parse date: {}", d));
+                let date_time = date_time.unwrap_or_else(|_| panic!("Unable to parse date: {}", d));
                 commit.date = Some(date_time);
-            },
+            }
         };
 
-        match get_one(&re_files, &line) {
+        match get_one(&re_files, line) {
             None => (),
             Some(f) => {
                 is_commit_ready = true;
                 commit.files = f.parse::<u32>().unwrap();
-            },
+            }
         };
 
-        match get_one(&re_inserts, &line) {
+        match get_one(&re_inserts, line) {
             None => (),
             Some(i) => commit.inserts = i.parse::<u32>().unwrap(),
         };
 
-        match get_one(&re_deletes, &line) {
+        match get_one(&re_deletes, line) {
             None => (),
             Some(d) => commit.deletes = d.parse::<u32>().unwrap(),
         };
@@ -68,7 +68,7 @@ pub fn to_commits(logs: &str) -> Vec<Commit> {
 }
 
 fn get_one(re: &Regex, line: &str) -> Option<String> {
-    let cap = re.captures(&line);
+    let cap = re.captures(line);
 
     match cap {
         None => None,
@@ -82,7 +82,7 @@ fn get_one(re: &Regex, line: &str) -> Option<String> {
 }
 
 fn get_two(re: &Regex, line: &str) -> Option<(String, String)> {
-    let cap = re.captures(&line);
+    let cap = re.captures(line);
 
     match cap {
         None => None,
