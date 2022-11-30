576a8f0aae330bf973815d6ffd458487b3f6bb74
Jonathan Neufeld <jneufeld@alumni.ubc.ca>
2022-11-30 02:13:07 -0800

    Having fun with `termion` and color

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,68 @@ use std::str;
 use crate::commit::Commit;
 use crate::histogram::Kind;
 
+use termion::{color, style};
+
 const HELP: &str = "\
 yeesh: simple stats for git repositories
 
 USAGE:
-  yeesh [-h]
+  yeesh [-h] [--hours] [--days]
 
 ARGS:
   -h, --help    Prints this message
+  --hours       (Optional) prints commit stats by hour of day
+  --days        (Optional) prints commit stats by weekday
 ";
 
+#[derive(Debug)]
+struct CliArgs {
+    hours: bool,
+    days: bool,
+}
+
 fn main() {
-    check_args();
+    let args = args_or_quit();
 
     let logs = get_git_logs();
+    let commits = parser::parse(&logs);
+    let commits = commits.unwrap();
 
-    match parser::parse(&logs) {
-        Ok(commits) => print_histogram(commits),
-        Err(why) => eprintln!("Error parsing git logs: {:?}", why),
+    if args.hours {
+        print_hours(&commits);
+    }
+
+    if args.days {
+        print_weekdays(&commits);
     }
 }
 
-fn check_args() {
+fn args_or_quit() -> CliArgs {
+    let args = parse_cli_args();
+
+    if !args.days && !args.hours {
+        print_help_and_quit();
+    }
+
+    args
+}
+
+fn parse_cli_args() -> CliArgs {
     let mut args = pico_args::Arguments::from_env();
 
     if args.contains(["-h", "--help"]) {
-        print!("{}", HELP);
-        process::exit(1);
+        print_help_and_quit();
     }
+
+    CliArgs {
+        hours: args.contains("--hours"),
+        days: args.contains("--days"),
+    }
+}
+
+fn print_help_and_quit() {
+    print!("{}", HELP);
+    process::exit(1);
 }
 
 fn get_git_logs() -> String {
@@ -58,26 +92,66 @@ fn get_git_logs() -> String {
     git_logs.to_string()
 }
 
-fn print_histogram(commits: Vec<Commit>) {
-    println!("By hour:");
+fn print_hours(commits: &Vec<Commit>) {
+    println!(
+        "{}{}By hour:{}",
+        style::Bold,
+        color::Fg(color::Magenta),
+        style::Reset
+    );
 
-    let by_hour = histogram::of_kind(Kind::ByHour, &commits);
+    let by_hour = histogram::of_kind(Kind::ByHour, commits);
 
     for hour in 1..24 {
         let count = by_hour.count_at(hour) as usize;
-        println!("{:02} | {}", hour, "-".repeat(count));
+
+        println!(
+            "{}{:02} {}| {}{}{}",
+            color::Fg(color::LightBlue),
+            hour,
+            color::Fg(color::White),
+            color::Fg(color::Yellow),
+            "-".repeat(count),
+            style::Reset,
+        );
     }
 
-    println!();
+    println!(
+        "\n{}total: {}{}\n",
+        style::Faint,
+        by_hour.len(),
+        style::Reset
+    );
+}
 
-    println!("By weekday:");
+fn print_weekdays(commits: &Vec<Commit>) {
+    println!(
+        "{}{}By weekday:{}",
+        style::Bold,
+        color::Fg(color::Magenta),
+        style::Reset
+    );
 
-    let by_weekday = histogram::of_kind(Kind::ByWeekday, &commits);
+    let by_weekday = histogram::of_kind(Kind::ByWeekday, commits);
 
     for weekday in 1..7 {
         let count = by_weekday.count_at(weekday) as usize;
-        println!("{:02} | {}", weekday, "-".repeat(count));
+
+        println!(
+            "{}{:02} {}| {}{}{}",
+            color::Fg(color::LightBlue),
+            weekday,
+            color::Fg(color::White),
+            color::Fg(color::Yellow),
+            "-".repeat(count),
+            style::Reset,
+        );
     }
 
-    println!("total: {}", by_hour.len());
+    println!(
+        "\n{}total: {}{}\n",
+        style::Faint,
+        by_weekday.len(),
+        style::Reset
+    );
 }
